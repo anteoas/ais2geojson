40c6b65873faa7656b865c425b98ccf526c6bc9b
Peder Refsnes <peder.refsnes@gmail.com>
2019-03-14 17:50:46 +0100

    csv: adds support for outputting csv
    - csv output
    - dont parse timestamps (half the execution time was spent doing this)

--- ais2geojson.c
+++ ais2geojson.c
@@ -1,5 +1,3 @@
-#define _XOPEN_SOURCE
-
 /* -----------------------------------------------------------------------
    Process AIS messages read from stdin and output JSON structure
    Copyright 2006 by Brian C. Lane
@@ -12,8 +10,60 @@
 #include <math.h>
 #include <errno.h>
 #include <time.h>
+#include <getopt.h>
 
 #include "aismsg_pos.h"
+
+
+static struct option options[] =
+{
+ {"geojson", 0, NULL, 'g'},
+ {"csv", 0, NULL, 'c'}
+};
+
+void writeGeoJSON(char *ais, aismsg_pos *pos) {
+  printf("{\"type\":\"Feature\",");
+  printf(
+         "" "\"geometry\":{"
+         ""   "\"type\":\"Point\","
+         ""   "\"coordinates\":[%0.7f,%0.7f]"
+         "" "},",
+         pos->long_ddd, pos->lat_dd);
+  printf(
+         "" "\"properties\":{"
+         ""   "\"mmsi\":\"%ld\",", pos->userid);
+  printf(""   "\"aistype\":%d,", pos->msgid);
+  if(pos->cog >= 0)   printf("\"cog\":%0.1f,", pos->cog);
+  if(pos->sog >= 0)   printf("\"sog\":%0.1f,", pos->sog);
+  if(pos->trueheading >= 0 && pos->trueheading < 360) {
+    printf("\"trueHeading\":%d,", pos->trueheading);
+  }
+  if(pos->smi >= 0)   printf("\"smi\":%d,", pos->smi);
+  // always print ts so we don't need to check if we need comma or not
+  if(pos->ts == 0) printf("\"ts\":false");
+  else {
+    // let's try to output ts as an integer first.
+    static char* endptr;
+    errno = 0;
+    long int ts = strtol(pos->ts, &endptr, 10);
+    if(errno == 0 && // no errors
+       (endptr + 1 == ais)) { // parsed the entire string (pos->ts+strlen(pos->ts) == ais)
+      printf("\"ts\":%ld", ts);
+    } else {
+      printf("\"ts\":\"%s\"", pos->ts);
+    }
+  }
+  printf("}}\n");
+}
+
+void writeCSV(char *ais, aismsg_pos *pos) {
+  printf("%s,%ld,%d,%0.7f,%0.7f,%0.1f,%0.1f,%d,%d\n",
+         pos->ts, pos->userid, pos->msgid,
+         pos->long_ddd, pos->lat_dd,
+         pos->cog, pos->sog,
+         pos->trueheading,
+         pos->smi);
+}
 
 // a buf typically looks like this:
 // 2015-09-20T17:00:00;!BSVDM,1,1,,B,13mBwe0P00Pa6:hTHmD>4?vT2<08,0*12
@@ -22,14 +72,26 @@ int main(int argc, char *argv[]) {
   aismsg_pos _pos, *pos; pos = &_pos;
   int err = 0;
 
-  char
-    config_smi = 1,
-    config_cog = 1,
-    config_trueheading = 1,
-    config_sog = 1;
-  struct tm tm_ts;
-  char buf_ts[20];
+  int geojson = 1;
+  void (*write)(char*,aismsg_pos*) = &writeGeoJSON;
 
+  int ch;
+  while((ch = getopt_long(argc, argv,"g:c:", options, NULL)) != -1) {
+    switch (ch) {
+    case 'g':
+      geojson = 1;
+      write = &writeGeoJSON;
+      break;
+    case 'c':
+      geojson = 0;
+      write = &writeCSV;
+      break;
+    }
+  }
+
+  if(!geojson) {
+    printf("timestamp,mmsi,ais_type,lon,lat,cog,sog,true_heading,smi\n");
+  }
 
   while( !feof(stdin) ) {
     if (fgets( buf, 255, stdin ) == NULL ) break;
@@ -42,54 +104,15 @@ int main(int argc, char *argv[]) {
     if((ais = strchr(buf, ';')) != NULL) {
       ais[0] = '\0'; // turn ; into '\0', same buffer nicely becomes two strings
       ais++; // point ais just past the first ;
-
-      if(strptime(buf, "%Y-%m-%dT%H:%M:%S", &tm_ts) != NULL) {
-        snprintf(buf_ts, 20, "%d", mktime(&tm_ts));
-        pos->ts = buf_ts;
-      } else {
-        pos->ts = buf; // timestamp now points to the buffer beginning (NULL-term were ; was)
-      }
-
+      pos->ts = buf; // timestamp now points to the buffer beginning (NULL-term were ; was)
     } else { // no ; found on line
       ais = buf;
       prefix = "";
     }
 
     if((err = buf2pos(ais, pos)) == 0) {
-      printf("{\"type\":\"Feature\",");
-      printf(
-             "" "\"geometry\":{"
-             ""   "\"type\":\"Point\","
-             ""   "\"coordinates\":[%0.7f,%0.7f]"
-             "" "},",
-             pos->long_ddd, pos->lat_dd);
-      printf(
-             "" "\"properties\":{"
-             ""   "\"mmsi\":\"%ld\",", pos->userid);
-      printf(""   "\"aistype\":%d,", pos->msgid);
-      if(config_cog && pos->cog >= 0)   printf("\"cog\":%0.1f,", pos->cog);
-      if(config_sog && pos->sog >= 0)   printf("\"sog\":%0.1f,", pos->sog);
-      if(config_trueheading && pos->trueheading >= 0 && pos->trueheading < 360) {
-          printf("\"trueHeading\":%d,", pos->trueheading);
-      }
-      if(config_smi && pos->smi >= 0)   printf("\"smi\":%d,", pos->smi);
-      // always print ts so we don't need to check if we need comma or not
-      if(pos->ts == 0) printf("\"ts\":false");
-      else {
-        // let's try to output ts as an integer first.
-        static char* endptr;
-        errno = 0;
-        long int ts = strtol(pos->ts, &endptr, 10);
-        if(errno == 0 && // no errors
-           (endptr + 1 == ais)) { // parsed the entire string (pos->ts+strlen(pos->ts) == ais)
-          printf("\"ts\":%ld", ts);
-        } else {
-          printf("\"ts\":\"%s\"", pos->ts);
-        }
-      }
-      printf("}}\n");
-    } // else printf("error in buf2aismsg_pos %d\n", err);
+           (*write)(ais, pos);
+    }// else printf("%s", ais);
   }
   return 0;
 }
-
